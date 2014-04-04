d13b42bdce60db64ad56f43350f3cb15f5f7bbe7
Fotis Gimian <fgimiansoftware@gmail.com>
2014-04-04 23:38:09 +1100

    Added Python 3 programming language support to setup.py

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ]
