bfe656da7d79f7ddd80f9dcf6ef9449d9dfc750a
Miguel Grinberg <miguelgrinberg50@gmail.com>
2015-09-17 11:41:11 -0700

    Release 1.6.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Migrate',
-    version='1.5.1',
+    version='1.6.0',
     url='http://github.com/miguelgrinberg/flask-migrate/',
     license='MIT',
     author='Miguel Grinberg',
