5db76032a6aaa2891682eaeea8bf7c0e74ae9523
Brian Cappello <briancappello@gmail.com>
2016-12-05 14:30:25 -0500

    support passing alembic context kwargs from constructor into init_app

--- flask_migrate/__init__.py
+++ flask_migrate/__init__.py
@@ -37,6 +37,7 @@ class Migrate(object):
         self.configure_callbacks = []
         self.db = db
         self.directory = directory
+        self.alembic_ctx_kwargs = kwargs
         if app is not None and db is not None:
             self.init_app(app, db, directory, **kwargs)
 
@@ -45,7 +46,8 @@ class Migrate(object):
         self.directory = directory or self.directory
         if not hasattr(app, 'extensions'):
             app.extensions = {}
-        app.extensions['migrate'] = _MigrateConfig(self, self.db, **kwargs)
+        app.extensions['migrate'] = _MigrateConfig(self, self.db,
+            **(kwargs if kwargs else self.alembic_ctx_kwargs))
 
     def configure(self, f):
         self.configure_callbacks.append(f)
