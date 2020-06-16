0259fb1b3fedd47995b3793ca4fca72953c92b14
James Addison <jay@jp-hosting.net>
2020-06-16 14:20:34 +0100

    Update flask env.py template: relocate import statement (mirrors 2a1ae1b)

--- flask_migrate/templates/flask/env.py
+++ flask_migrate/templates/flask/env.py
@@ -5,6 +5,7 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
+from flask import current_app
 
 from alembic import context
 
@@ -21,7 +22,6 @@ logger = logging.getLogger('alembic.env')
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from flask import current_app
 config.set_main_option(
     'sqlalchemy.url',
     str(current_app.extensions['migrate'].db.engine.url).replace('%', '%%'))
